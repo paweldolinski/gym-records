381d3130a9ead1dcaad794dfea6d473c2f6f7d7d
paweldolinski <pawel.x.dolinski@viivhealthcare.com>
2025-03-28 11:06:56 +0100

    domain fix

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const domain = "http://sokol-krzywin.pl";
+const domain = "https://sokol-krzywin.pl";
 
 export const sendVerificationEmail = async (email: string, token: string) => {
 	const confirmationLink = `${domain}/verify-email?token=${token}`;
