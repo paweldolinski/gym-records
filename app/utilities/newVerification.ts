db825b3bcb53cf33174c6934818a2a9b8bb71b68
paweldolinski <pawel.x.dolinski@viivhealthcare.com>
2025-03-31 15:12:15 +0200

    test

--- app/utilities/newVerification.ts
+++ app/utilities/newVerification.ts
@@ -5,11 +5,11 @@ import mongoose from "mongoose";
 import {connectDB} from "../../lib/mongodb";
 
 export const newVerification = async (token: string) => {
-	console.log("Checking DB connection...");
-	if (mongoose.connection.readyState !== 1) {
-		console.log("MongoDB not connected! Trying to reconnect...");
-		await connectDB();  // Spróbuj ponownie połączyć się z MongoDB
-	}
+	// console.log("Checking DB connection...");
+	// if (mongoose.connection.readyState !== 1) {
+	// 	console.log("MongoDB not connected! Trying to reconnect...");
+	// 	await connectDB();  // Spróbuj ponownie połączyć się z MongoDB
+	// }
 
 	try {
 		const existingToken = await getVerificationTokenByToken(token);
