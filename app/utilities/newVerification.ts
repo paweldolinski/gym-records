7febf245aac89b8f68505d54fbbe40bb2ab1753f
paweldolinski <pawel.x.dolinski@viivhealthcare.com>
2025-03-31 14:59:18 +0200

    logs

--- app/utilities/newVerification.ts
+++ app/utilities/newVerification.ts
@@ -3,6 +3,12 @@ import { deleteVerificationToken, getVerificationTokenByToken } from "./token";
 import { findUser, verifyEmail } from "./user";
 
 export const newVerification = async (token: string) => {
+	console.log("Checking DB connection...");
+	if (mongoose.connection.readyState !== 1) {
+		console.log("MongoDB not connected! Trying to reconnect...");
+		await connectDB();  // Spróbuj ponownie połączyć się z MongoDB
+	}
+
 	try {
 		const existingToken = await getVerificationTokenByToken(token);
 
