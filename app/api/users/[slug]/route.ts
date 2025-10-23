139c91a5fe8bf5b09530059fc44e83616f7ad047
paweldolinski <pawel.x.dolinski@viivhealthcare.com>
2025-10-23 12:24:17 +0200

    fixes for deploy

--- app/api/users/[slug]/route.ts
+++ app/api/users/[slug]/route.ts
@@ -1,23 +1,19 @@
+// app/api/users/[slug]/route.ts
+import { NextResponse } from "next/server";
 import { findUserBySlug } from "@/utilities/user";
-import { NextResponse, NextRequest } from "next/server";
 import { connectDB } from "../../../../lib/mongodb";
 
-export async function GET(
-	request: NextRequest,
-	{ params }: { params: { slug: string } },
-) {
+type Params = { params: { slug: string } };
+
+export async function GET(req: Request, { params }: Params) {
 	const { slug } = params;
 
 	try {
 		await connectDB();
-
 		const user = await findUserBySlug(slug);
 
 		if (!user) {
-			return NextResponse.json(
-				{ message: "Użytkownik nie znaleziony" },
-				{ status: 404 },
-			);
+			return NextResponse.json({ message: "Użytkownik nie znaleziony" }, { status: 404 });
 		}
 
 		return NextResponse.json(user, { status: 200 });
